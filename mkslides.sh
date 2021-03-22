90af79fa2dc7b36074d2748d9bfa085361f30caa
AnttiHaerkoenen <antth@uef.fi>
2021-03-22 11:19:26 +0200

    slides 5

--- mkslides.sh
+++ mkslides.sh
@@ -2,5 +2,6 @@
 
 cd ./article/slides
 pandoc -s --mathml -i -t revealjs slides-seminar-2021.md -o slides-seminar-2021.html --slide-level 2 --include-in-header custom.css
+pandoc -s --mathml -i -t pptx slides-seminar-2021.md -o slides-seminar-2021.pptx --slide-level 2
 sed 's/black.css/sky.css/g' slides-seminar-2021.html -i slides-seminar-2021.html
 cd ..
